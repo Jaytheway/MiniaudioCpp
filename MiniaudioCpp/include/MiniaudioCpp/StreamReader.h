19e0f3afbe04adcb09158a84b982277fb228dc25
Jaroslav <music.jp.production@gmail.com>
2024-09-25 11:22:26 +0300

    Added missing `SetStreamPosition` function to StreamReader

--- MiniaudioCpp/include/MiniaudioCpp/StreamReader.h
+++ MiniaudioCpp/include/MiniaudioCpp/StreamReader.h
@@ -34,6 +34,8 @@ namespace JPL
 		explicit StreamReader(InternalReader* reader) { JPL_ASSERT(reader != nullptr); mReader = reader; }
 
 		JPL_INLINE size_t GetStreamPosition() const { return mReader.GetStreamPosition(); }
+		JPL_INLINE void SetStreamPosition(size_t position) { mReader.SetStreamPosition(position);
+		}
 		
 		void ReadData(char* destination, size_t size) { mReader.ReadData(destination, size); }
 		
